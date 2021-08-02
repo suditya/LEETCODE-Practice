658cf2e9bb8d9c6f56e2cf7aaf1aa46a8d4c157e
suditya <70383984+suditya@users.noreply.github.com>
2021-08-02 18:47:51 +0530

    Time: 12 ms (69.64%), Space: 5.8 MB (88.49%) - LeetHub

---
+++ power-of-three/power-of-three.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+  
+    bool isPowerOfThree(int n) 
+    {
+        
+        if(n<=0)
+            return false;
+        else if(n==1)
+            return true;
+        else if( (n%3) !=0) return false;
+        else 
+        {
+            return isPowerOfThree(n/3);
+        }
+    }
+};
