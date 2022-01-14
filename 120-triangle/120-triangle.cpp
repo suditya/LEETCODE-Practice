d6e1b54034be4f9c75cdae59f6d21694aa7c6a3d
suditya <70383984+suditya@users.noreply.github.com>
2022-01-14 12:23:15 +0530

    Time: 20 ms (5.09%), Space: 8.8 MB (37.47%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int n;
+    int dp[205][207];
+    int f(int i, int index,vector<vector<int>>& t)
+    {
+        
+        if(index >= t[i].size() ) return INT_MAX;
+        else if(i==n-1)
+            return t[n-1][index];
+        else if(dp[i][index]!=-1) return dp[i][index];
+        else
+        {
+            cout<<i<<" ";
+            return dp[i][index]=(t[i][index]+min(f(i+1,index,t),f(i+1,index+1,t)));
+        }
+    }
+    
+    int minimumTotal(vector<vector<int>>& triangle) 
+    {
+        cout<<"\n";
+        for(int i=0;i<205;i++)
+        {
+            for(int j=0;j<207;j++)
+                dp[i][j]=-1;
+        }
+        n=triangle.size();
+        
+        return f(0,0,triangle);
+    }
+};
