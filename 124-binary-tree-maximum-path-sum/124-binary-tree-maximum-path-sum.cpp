70bae0d1cf07202dcf8a1958fe2511ab84448ef5
suditya <70383984+suditya@users.noreply.github.com>
2022-01-22 20:48:35 +0530

    Time: 42 ms (26.62%), Space: 27.6 MB (92.04%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -12,8 +12,8 @@
 class Solution {
 public:
     unordered_map<TreeNode*, int> dp;
-    int ans;
-    int f(TreeNode* root,int previous=0)
+    // int ans;
+    int f(TreeNode* root,int &res)
     {
         if(root==NULL) return 0;
         // else if(root->left==NULL and root->right==NULL)
@@ -23,21 +23,19 @@ public:
         //     return dp[root];
         else
         {
-            int l=max(0,f(root->left));
-            int r=max(0,f(root->right));
-            // ans=max(a,b);
-            // int a=max({l,r,l+r,0})+root->val;
-            // int b=max(l,r);
-            // // cout<<a<<" "<<b<<"\n";
-            // return dp[root]=max(a,b);
-            ans=max(ans,(l+r+(root->val)));
-            return max({l,r})+(root->val);
+            int l=max(0,f(root->left,res));
+            int r=max(0,f(root->right,res));
+            int pass_on=max(l,r)+root->val;
+            int ans=max(pass_on,l+r+root->val);
+            res=max(res,ans);
+            return pass_on;
         }
     }
     
     int maxPathSum(TreeNode* root) 
     {
-        ans=INT_MIN;
-      return (f(root),ans);   
+        int res=INT_MIN;
+        int x=(f(root,res));
+        return res;
     }
 };
