3499b93ea758b8a2341633339cae111b634d1154
suditya <70383984+suditya@users.noreply.github.com>
2022-02-22 22:48:44 +0530

    Time: 14 ms (12.92%), Space: 9.3 MB (94.12%) - LeetHub

---
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+   
+    int dp[400];
+    int f(int start ,vector<int>& day, vector<int>& cost)
+    {
+
+        if(start > 365) return 0;
+        else
+        {
+            int real_start=0;
+            int i=0;
+            for(;i<day.size();i++)
+            {
+                if(day[i]>=start)
+                {
+                    real_start=day[i];
+                    break;
+                }
+            }
+           
+            if(i>=day.size()) return 0;
+            if(dp[real_start]!=-1) return dp[real_start];
+            else
+    return dp[real_start]= 
+     min(f(real_start+1,day,cost)+cost[0],min(f(real_start+7,day,cost)+cost[1],f(real_start+30,day,cost)+cost[2]));             
+        }
+    }
+    
+    int mincostTickets(vector<int>& days, vector<int>& costs) 
+    {
+        for(int i=0;i<400;i++)
+        {
+            dp[i]=-1;
+        }
+        dp[365]=min(costs[0],min(costs[1],costs[2]));
+        int ans=f(1,days,costs);
+        return ans;
+        
+    }
+};
