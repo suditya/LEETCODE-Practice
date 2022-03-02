d1d96a048d069897e2fb8ec15180edde82366240
suditya <70383984+suditya@users.noreply.github.com>
2022-03-02 12:00:33 +0530

    Time: 28 ms (74.44%), Space: 8.9 MB (97.28%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,50 +1,18 @@
 class Solution {
 public:
-    int c;
-    vector<vector<int>> ans;
-    int realK;
-    // map<int,int> dp;
-    
-    void dfs(int n,int k,vector<int> path,int x=0)
-    {
-        if(path.size()==realK)
-        {
-            ans.push_back(path);
-            return ;
-        }
-        else
-        {
-            ++c;
-            // cout<<"\n"<<x<<" ";
-            for(int i=1+x;i<=n;i++)
-            {
-                // int prev=0;
-                // if(path.size() > 0) prev=path[path.size()-1]; 
-                // if(mp[i]==0 )
-                // {
-                    // cout<<i<<" ";
-                    path.push_back(i);
-                    // dp[]
-                    // mp[i]=1;
-                    dfs(n,k,path,i);
-                    // mp[i]=0;
-                    path.pop_back();
-                    // k=k+1;
-                // }
-                // cout<<"\n";
-            }
-        }
-    }
-    
-    vector<vector<int>> combine(int n, int k) 
-    {
-        c=0;
-        realK=k;
-        vector<int> path;
-        // map<int,int> mp;
-        // for(int i=1;i<=n;i++) mp[i]=0;
-        dfs(n,k,path);
-        cout<<c<<" ";
-        return ans;
-    }
+    vector<vector<int>> combine(int n, int k) {
+		vector<vector<int>> result;
+		int i = 0;
+		vector<int> p(k, 0);
+		while (i >= 0) {
+			p[i]++;
+			if (p[i] > n) --i;
+			else if (i == k - 1) result.push_back(p);
+			else {
+			    ++i;
+			    p[i] = p[i - 1];
+			}
+		}
+		return result;
+	}
 };
