79fad99eabfe327c8954105306a4609027ed6001
suditya <70383984+suditya@users.noreply.github.com>
2022-03-10 13:06:37 +0530

    Time: 46 ms (54.41%), Space: 13.8 MB (98.99%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,52 +1,13 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        def f(s:str,n:int,dp):
-            if dp[n]!=-1:
-                return dp[n]
-            if n==0:
-                return 0
-            elif s[n-1]==0:
-                return f(s,n-1,dp)
-            elif n==1:
-                return 1
-            elif s[n-1]==0 and s[n-2]==0:
-                return 0
-            elif n==2:
-                x=s[n-2]+s[n-1]
-                num=int(x)
-                print(num,x)
-                if int(s[n-2])>=3 and s[n-1]=='0':
-                    print("hi2")
-                    return 0
-                elif num==10 or num==20:
-                    return 1
-                elif num<=26:
-                    return 2
-                else:
-                    return 1
+        dp = { len(s) : 1}
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "0":
+                 dp[i] = 0
             else:
-                x=s[n-2]+s[n-1]
-                num=int(x)
-                print(num,x)
-                if int(s[n-2])>=3 and s[n-1]=='0':
-                    print("hi2")
-                    return 0
-                if num==10 or num==20:
-                    dp[n]=f(s,n-2,dp)
-                    return dp[n]
-                elif x=="00":
-                    return 0
-                elif s[n-2]=='0':
-                    print("hi")
-                    dp[n]=f(s,n-1,dp)
-                    return dp[n]
-                elif num<=26:
-                    dp[n]=f(s,n-1,dp)+f(s,n-2,dp)
-                    return dp[n]
-                else:
-                    dp[n]=f(s,n-1,dp)
-                    return dp[n]
-        dp = [-1 for i in range(len(s)+5)]
-        if s[0]=='0':
-            return 0
-        return f(s,len(s),dp)
+                 dp[i] = dp[i + 1]
+            if (i + 1 < len(s) and (s[i] == "1" or
+                  s[i] == "2" and s[i + 1] in "0123456")):
+                    dp[i] += dp[i + 2]
+        return dp[0]
+        
