1eec7be8b01a33e095b359c08a2c0cf90edd0f24
suditya <70383984+suditya@users.noreply.github.com>
2022-03-19 13:16:31 +0530

    Time: 20 ms (10.37%), Space: 13.9 MB (26.70%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& a) 
+    {
+        int n=a.size();
+        int mini=a[n-1],maxi=a[n-1];
+        int ans=a[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            int tmax,tmin;
+            tmax=max({a[i],a[i]*maxi,a[i]*mini});
+            tmin=min({a[i],a[i]*mini,a[i]*maxi});
+            // cout<<tmax<<" "<<tmin<<"\n";
+            maxi=tmax;
+            mini=tmin;
+            ans=max({ans,tmax,tmin});
+        }
+        // cout<<maxi<<" "<<mini;
+        return ans;
+    }
+};
