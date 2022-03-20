b7f0f72ad84fc0a70ea79c92483438b2757832c2
suditya <70383984+suditya@users.noreply.github.com>
2022-03-20 23:52:22 +0530

    Time: 372 ms (33.04%), Space: 16.7 MB (53.29%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int jump(vector<int>& a) 
+    {
+        int n=a.size();
+        int dp[n];
+        dp[n-1]=0;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(i+a[i] >= n-1)
+                dp[i]=1;
+            else
+            {
+                int ans=INT_MAX-1;
+                for(int j=i+1;j<n and j<= (i+a[i]);j++)
+                {
+                    ans=min(ans,dp[j]);
+                }
+                // if(ans==INT_MAX)
+                //     dp[i]=INT_MAX;
+                // else
+                    dp[i]=ans+1;
+            }
+        }
+        return dp[0];
+    }
+};
