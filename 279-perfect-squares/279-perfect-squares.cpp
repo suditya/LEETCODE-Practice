a1918f5ab2db340392806978ea31ae9eb5dd172a
suditya <70383984+suditya@users.noreply.github.com>
2022-03-21 15:21:59 +0530

    Time: 102 ms (79.25%), Space: 5.9 MB (91.83%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+   
+    int numSquares(int n) 
+    {
+        //using bottom up approach or dynamic programming .
+        if(n<=0)
+             return 0;
+        if(n==1)
+             return 1;
+        if(n==2)
+            return 2;
+        else
+        {
+            int dp[n+1];
+            for(int i=0;i<=n;i++) dp[i]=INT_MAX;
+            dp[0]=0;
+            dp[1]=1;
+            dp[2]=2;
+            // dp[3]=3;
+            // dp[4]=1;
+            for(int i=3;i<=n;i++)
+            {
+                for(int j=1;j*j<=i;j++) // checking all possible answers 
+                {
+                    dp[i]=min(dp[i],dp[i-(j*j)]+1);
+                }
+                // cout<<i<<" "<<dp[i]<<"\n";
+            }
+            return dp[n];
+        }
+    }
+   
+};
