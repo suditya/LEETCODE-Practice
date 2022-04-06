ab91be97c20edee771b4dc2e70349d602a28e41c
suditya <70383984+suditya@users.noreply.github.com>
2022-04-06 23:15:37 +0530

    Time: 478 ms (42.86%), Space: 10.4 MB (74.83%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int threeSumMulti(vector<int>& arr, int target) 
+    {
+        	int n = size(arr), ans = 0, mod = 1000000007;
+	sort(begin(arr), end(arr));
+	for(int i = 0; i < n - 2; i++){
+		int T = target - arr[i],  j = i + 1, k = n - 1;
+		while(j < k){
+			if(arr[j] + arr[k] == T){
+				if(arr[j] == arr[k]){
+					ans = (ans + (((k - j) * (k - j + 1)) >> 1) % mod) % mod;  break;
+				} 
+				else{
+					int l = 1, r = 1;
+					while(j + 1 < k && arr[j] == arr[j + 1]) j++, l++;
+					while(k - 1 >= j && arr[k] == arr[k - 1]) k--, r++;
+					ans += (l * r) % mod;
+					j++, k--;
+				}                    
+			}
+			else if(arr[j] + arr[k] < T) j++;
+			else k--;
+		}
+	}
+	return ans;
+   
+    }
+};
