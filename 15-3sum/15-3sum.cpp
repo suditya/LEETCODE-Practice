6c60cb25d539c6b9cc40f9968feb319175b388ac
suditya <70383984+suditya@users.noreply.github.com>
2022-04-06 17:52:48 +0530

    Time: 122 ms (55.02%), Space: 23.7 MB (25.68%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,59 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& a) 
+    vector<vector<int>> threeSum(vector<int>& num) 
     {
-        set<vector<int>> ans;
-        int n=a.size();
-        if(n<3) return {};
-        sort(a.begin(),a.end());
-        // for(auto x:a) cout<<x<<" ";
-        // cout<<"\n";
-        for(int i=0;i<n;i++)
-        {
-            int z=a[i];
-            int x=i+1;
-            int y=n-1;
-            z=(-1)*z;
-            // cout<<z<<"\n";
-            while(x<y)
-            {
-                
-                if( x==i )
-                {
-                    // while(x!=i)
-                    ++x;
-                }
-                if(y==i)
-                {
-                    --y;  
-                }
-                if(x>=y) break;
-                // cout<<a[x]<<" "<<a[y]<<" "<<(-z)<<"\n";
-                if(a[x]+a[y] == z)
-                {
-                    // cout<<"hi upar waala\n";
-                    vector<int> v={a[x],a[y],((-1)*z)};
-                    sort(v.begin(),v.end());
-                    ans.insert(v);
-                    ++x;
-                    --y;
-                }
-                else if(a[x]+a[y] < z)
-                {
-                    ++x;
-                }
-                else
-                {
-                    --y;
-                }
-            }
+        vector<vector<int>> res; 
+        sort(num.begin(), num.end()); 
+        
+        // moves for a
+        for (int i = 0; i < (int)(num.size())-2; i++) {
             
+            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
+                
+                int lo = i+1, hi = (int)(num.size())-1, sum = 0 - num[i];
+                
+                while (lo < hi) {
+                    if (num[lo] + num[hi] == sum) {
+                        
+                        vector<int> temp; 
+                        temp.push_back(num[i]); 
+                        temp.push_back(num[lo]); 
+                        temp.push_back(num[hi]); 
+                        res.push_back(temp);
+                        
+                        while (lo < hi && num[lo] == num[lo+1]) lo++;
+                        while (lo < hi && num[hi] == num[hi-1]) hi--;
+                        
+                        lo++; hi--;
+                    } 
+                    else if (num[lo] + num[hi] < sum) lo++;
+                    else hi--;
+               }
+            }
         }
-        vector<vector<int>> real;
-        for(auto x: ans)
-        {
-            real.push_back(x);
-        }
-        return real;
+        return res;    
     }
 };
