b9903c0e75e9fb61400bd816c068f4a3b67ae6d1
suditya <70383984+suditya@users.noreply.github.com>
2022-06-20 14:32:44 +0530

    Time: 10 ms (68.70%), Space: 8.8 MB (71.93%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    map<int,bool> dp;
+    int dp[305];
     bool f(string s,vector<string>& dict,int start)
     {
-        if(start == s.size()) return true;
-        else if (start > s.size()) return false;
-        else if(dp.find(start)!=dp.end())
+        if(start==s.size()) return true;
+        else if(dp[start]!=-1) 
+        {
+            // cout<<"hi";
             return dp[start];
+        }
         else
         {
-            for(int i=0;i<dict.size();i++)
+            for(auto d:dict)
             {
-                int d=dict[i].size();
-                string sx=(s.substr(start,d));
-                // cout<<sx<<" "<<start<<" "<<d<<"\n";
-                // cout<<sx<<" "<<dict[i]<<" "<<start<<" "<<d<<"\n";
-                if(s[start]==dict[i][0] and (sx==dict[i]))
+                int l=d.size();
+                string temp=s.substr(start,l);
+                // cout<<temp<<" "<<d<<"\n";
+                if(temp==d)
                 {
-                    // cout<<sx<<" "<<dict[i]<<" "<<start<<" "<<d<<" -passed \n";
-                    if(f(s,dict,(start+d)))
-                        return dp[start]=true;
+                    if(f(s,dict,start+l)==true) return dp[start]=true;
                 }
             }
             return dp[start]=false;
         }
     }
     
-    
     bool wordBreak(string s, vector<string>& wordDict) 
     {
+        memset(dp,-1,sizeof(dp));
         return f(s,wordDict,0);   
     }
 };
