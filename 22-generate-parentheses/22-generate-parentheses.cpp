c84c1fe8f9a1eea9e207c0ab3bb0dd9f516c5047
suditya <70383984+suditya@users.noreply.github.com>
2022-06-24 15:14:55 +0530

    Time: 4 ms (72.67%), Space: 7.3 MB (97.56%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,72 +1,13 @@
 class Solution {
 public:
-    bool isValid(string s) {
-    stack<char> st;
-    for(char c : s){
-        if(c == '('){
-            st.push(c);
-        }else{
-            if(st.empty()) return false;
-            if(c == ')' && st.top() != '(') return false;
-            st.pop();
-        }
-    }
-    return st.empty();
-    }
-    
-    vector<string> para;
-    
-    void generate(int open,int close,string path)
-    {
-        if(open==0 and close==0)
-        {
-            if(path[0]!=')' and path[path.size()-1]!='(')
-            para.push_back(move(path));
-            return ;
-        }
-        else
-        {
-            if(path.size()==0 or (open==close))
-            {
-                path.push_back('(');
-                generate(open-1,close,path);
-            }
-            else if(open>0 and close>0)
-            {
-                path.push_back('(');
-                generate(open-1,close,path);
-                path.pop_back();
-                path.push_back(')');
-                generate(open,close-1,path);
-            }
-            else
-            {
-                if(open>0)
-                {
-                    for(int i=0;i<open;i++)
-                    path.push_back('(');
-                    generate(0,close,path);
-                }
-                else
-                {
-                    for(int i=0;i<close;i++)
-                    path.push_back(')');
-                    generate(open,0,path);
-                }
-            }
-        }
-    }
-    
-    vector<string> generateParenthesis(int n) 
-    {
-        string path;
-        generate(n,n,path);
-        vector<string> ans;
-        for(auto s:para)
-        {
-            if(isValid(s)==true)
-                ans.push_back(s);
-        }
-        return ans;
-    }
+    vector<string> generateParenthesis(int n) {
+	vector<string> ans;
+	solve(0, 0, n, "", ans);
+	return ans;
+}
+void solve(int start, int close, int n, string s, vector<string>& ans) {
+	if(size(s) == 2*n) ans.push_back(move(s));  // use move to directly push the constructed string into vector instead of creating a copy
+	if(start < n) solve(start + 1, close, n, s + '(', ans);
+	if(close < start) solve(start, close + 1, n, s + ')', ans);
+}
 };
