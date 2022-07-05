b84a11a58671981056070c458282fbcba246a2c6
suditya <70383984+suditya@users.noreply.github.com>
2022-07-05 12:08:19 +0530

    Added solution - LeetHub

--- Punish the Students - GFG/punish-the-students.cpp
+++ Punish the Students - GFG/punish-the-students.cpp
@@ -12,7 +12,7 @@ class Solution{
         for(int i=0;i<n;i++)
         {
            
-            for(int j=0;j<n-1;j++)
+            for(int j=0;j<n-1-i;j++)
             {
                 if(roll[j]>roll[j+1])
                 {
