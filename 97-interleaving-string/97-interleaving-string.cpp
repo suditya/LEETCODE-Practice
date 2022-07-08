3d5f51a2e23e4338822e91a26a5eedbe4f107209
suditya <70383984+suditya@users.noreply.github.com>
2022-07-08 12:11:41 +0530

    Time: 160 ms (5.00%), Space: 42.8 MB (7.55%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,41 +1,52 @@
 class Solution {
 public:
-    int n;
-    int m;
-    map<pair<int,int>,int> dp;
-    bool f(string s1,string s2,string s3,int x,int y,int z)
+    int a, b , c;
+    int dp[102][102][202];
+    bool f(string s1, string s2, string s3,int i,int j,int k)
     {
-        if(z>= s3.size())
+        if(i==a and b==j)
+        {
             return true;
-        else if(dp.find({x,y})!=dp.end())
-            return dp[{x,y}];
+        }
+        else if(dp[i][j][k]!=-1) return dp[i][j][k];
+        else if(i==a)
+        {
+            if(s2[j]==s3[k]) return dp[i][j][k]=f(s1,s2,s3,i,j+1,k+1);
+            else return dp[i][j][k]=false;
+        }
+        else if(j==b)
+        {
+           if(s1[i]==s3[k]) return dp[i][j][k]=f(s1,s2,s3,i+1,j,k+1);
+           else return dp[i][j][k]=false;
+        }
         else
         {
-            if(x<n and s3[z]==s1[x] and y<m and s3[z]==s2[y])
+            if(s1[i]==s3[k]  and s2[j]==s3[k])
             {
-                return dp[{x,y}]=(f(s1,s2,s3,x+1,y,z+1)|f(s1,s2,s3,x,y+1,z+1));
+                return dp[i][j][k]=(f(s1,s2,s3,i+1,j,k+1)|f(s1,s2,s3,i,j+1,k+1));
             }
-            else if(x<n and s3[z]==s1[x])
+            else if(s1[i]==s3[k])
             {
-                return dp[{x,y}]=f(s1,s2,s3,x+1,y,z+1);
+                return dp[i][j][k]=f(s1,s2,s3,i+1,j,k+1);
             }
-            else if(y<m and s3[z]==s2[y])
+            else if(s2[j]==s3[k])
             {
-                return dp[{x,y}]=f(s1,s2,s3,x,y+1,z+1);
+                return dp[i][j][k]=f(s1,s2,s3,i,j+1,k+1);
             }
-            else
-                return false;
+            else return dp[i][j][k]=false;
         }
+        
     }
     
     bool isInterleave(string s1, string s2, string s3) 
     {
-        if(s3.size()!= (s1.size()+s2.size())) return false;
-        // int x=0,y=0,z=0;
-        n=s1.size();
-        m=s2.size();
-        // int p=s3.size();
-        return f(s1,s2,s3,0,0,0);
-        
+        // int a,b,c;
+        a=s1.size(),b=s2.size(),c=s3.size();
+        if( a+b  ==c )
+        {
+            memset(dp,-1,sizeof(dp));
+            return f(s1,s2,s3,0,0,0);
+        }
+        else return false;
     }
 };
