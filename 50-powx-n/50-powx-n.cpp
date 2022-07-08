af6ea117514c0748e783f1345defb77400718ceb
suditya <70383984+suditya@users.noreply.github.com>
2022-07-08 12:57:49 +0530

    Time: 0 ms (100.00%), Space: 6 MB (18.13%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    
     double f(double x, int n)
     {
         if(n==0) return 1;
-        else if (n==1) return x;
+        else if(n==1) return x;
         else
         {
-            double y=f(x,n/2);
-            if(n%2==0) return (y*y);
-            else return (y*y*x);
+            double temp=f(x,n/2);
+            if((n&1)==0)
+            {
+                return temp*temp;
+            }
+            else
+            {
+                return temp*temp*x;
+            }
         }
     }
     
     double myPow(double x, int n) 
     {
-         int m=abs(n);
-        double ans=f(x,m);
-        // if(ans==0) return 0;
-        if(n<0)
-        {
-            return (1/ans);
-        }
-        // else if(ans==0) return 0;
+        if(n>=0)
+        return f(x,n);
         else
-            return ans;
+            return (1/f(x,n));
     }
 };
