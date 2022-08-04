902a19591cee2cd07b582935b33756916ecc10d8
suditya <70383984+suditya@users.noreply.github.com>
2022-08-05 00:51:45 +0530

    Time: 32 ms (6.77%), Space: 7.7 MB (39.34%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
@@ -8,39 +8,10 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode* node)
-    {
-        if(node==NULL) return NULL;
-        else if(node->next ==NULL) return node;
-        else
-        {
-            ListNode* temp=reverse(node->next);
-            node->next->next=node;
-            node->next=NULL;
-            return temp;
-        }
-    }
     
     void deleteNode(ListNode* node) 
     {
-        vector<int> ele;
-        ListNode* temp=node->next;
-        while(temp!=NULL)
-        {
-            ele.push_back(temp->val);
-            // cout<<((temp->val))<<" ";
-            temp=temp->next;
-        }
-        int n=ele.size();
-        for(int i=0;i<n;i++)
-        {
-            // cout<<ele[i]<<" ";
-            node->val=ele[i];
-           
-            if(i==n-1) node->next=NULL;
-             node=node->next;
-        }
-        // node->next=NULL;
-        return ;
+       node->val = node->next->val;
+        node->next = node->next->next;
     }
 };
