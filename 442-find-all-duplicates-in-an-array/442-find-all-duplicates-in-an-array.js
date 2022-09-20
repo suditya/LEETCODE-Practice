9f2be10263aee2ab8117510507d03e5731f99bfb
suditya <70383984+suditya@users.noreply.github.com>
2022-09-20 22:33:27 +0530

    Time: 204 ms (30.72%), Space: 60.6 MB (12.22%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.js
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.js
@@ -9,12 +9,6 @@ var findDuplicates = function(nums) {
     nums.forEach((char)=>
     {
         if (myMap.has(char) == true) {
-        // let prevCount = myMap.get(char);
-        // myMap.set(char, prevCount + 1);
-        // if (prevCount + 1 > ans) {
-        //     ans = prevCount + 1;
-        //     ansChar = char;
-            
             duplicate.push(char);
         }
         
